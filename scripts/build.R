95bf74d49520276e7ee35e9e9406ba1cde776574
Poorani <poorani.subramanian@nih.gov>
2018-03-08 16:16:34 -0500

    add source file to function help
    Former-commit-id: c70730109db3d3df6263db3872754daddf629408
    Former-commit-id: 8b4ee2f053768aa9703f7d98d7eff3171426f150
    Former-commit-id: 5e1d94835b3ac45f1f1373346853e5df6c7281b3

--- scripts/build.R
+++ scripts/build.R
@@ -2,12 +2,12 @@ library(devtools)
 library(rmarkdown)
 
 ## Build package
-document(roclets=c('rd', 'collate', 'namespace'))
-install(args = c("--preclean", "--no-multiarch", "--with-keep.source"))
-
-## Imported packages - can check DESCRIPTION
-ns <- scan("NAMESPACE", sep="\n", what = character())
-importedpackages <- unique(stringr::str_match(ns, "import.*\\((.*?)[\\,\\)]")[,2])
+# document(roclets=c('rd', 'collate', 'namespace'))
+# install(args = c("--preclean", "--no-multiarch", "--with-keep.source"))
+#
+# ## Imported packages - can check DESCRIPTION
+# ns <- scan("NAMESPACE", sep="\n", what = character())
+# importedpackages <- unique(stringr::str_match(ns, "import.*\\((.*?)[\\,\\)]")[,2])
 
 ## Documentation
 
@@ -32,14 +32,14 @@ file.copy(mdfile, Rmdfile, overwrite = TRUE )
 render(Rmdfile)
 file.remove(Rmdfile)
 
-## User docs
-render("doc/user_doc.Rmd", output_format = "github_document")
-file.rename("doc/user_doc.md", "doc/github_doc.md")
-render("doc/user_doc.Rmd", output_format = "html_document", output_file = "datavis16s_pipeline.html")
-system2(command = "sed" , args=c('-i.bak', '\'s/[\\“\\”]/\"/g\'', "doc/datavis16s_pipeline.html"))
-file.remove("doc/datavis16s_pipeline.html.bak")
-file.rename("doc/github_doc.md", "doc/user_doc.md")
-file.remove("doc/user_doc.html")
+# ## User docs
+# render("doc/user_doc.Rmd", output_format = "github_document")
+# file.rename("doc/user_doc.md", "doc/github_doc.md")
+# render("doc/user_doc.Rmd", output_format = "html_document", output_file = "datavis16s_pipeline.html")
+# system2(command = "sed" , args=c('-i.bak', '\'s/[\\“\\”]/\"/g\'', "doc/datavis16s_pipeline.html"))
+# file.remove("doc/datavis16s_pipeline.html.bak")
+# file.rename("doc/github_doc.md", "doc/user_doc.md")
+# file.remove("doc/user_doc.html")
 
 ## README
 # render("README.Rmd")
