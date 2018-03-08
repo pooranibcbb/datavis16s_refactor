9da8f0dfba8bae8c24b04d5c3770309ecab56a97
Poorani <poorani.subramanian@nih.gov>
2018-03-08 14:53:42 -0500

    suppress ggplotly message
    Former-commit-id: 9afdeec761b20c7b5c6c2bf141fc6fe3a2d669cc
    Former-commit-id: 85ea16ac3b46b83f5e303ddcf91206c8cf28e278
    Former-commit-id: fa2379a9675e44283a66ba364b7a2d85051cc158

--- scripts/test.R
+++ scripts/test.R
@@ -1,16 +1,14 @@
 library(datavis16s)
+## mothur
 
-amp <- readindata(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/mothur.biom")
 outdir <-  "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/mothur"
 dir.create(outdir, showWarnings = FALSE)
-#ggg <- allgraphs(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/mothur.biom", outdir = outdir, sampdepth = 5713)
-#ggg <- pcoaplot(amp=amp, sampdepth = 47052, outdir=outdir)
-#morphheatmap(outdir = outdir, amp = amp, taxlevel = c("Family", "seq"))
+ggg <- allgraphs(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/mothur.biom", outdir = outdir, sampdepth = 5713)
 
+outdir <-  "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/greengenes"
+dir.create(outdir, showWarnings = FALSE)
+ggg <- allgraphs(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/greengenesOTU_table.biom", outdir = outdir, sampdepth = 47052)
 
-withCallingHandlers({
-  ggplotly(ggiris)
-}, message=function(c) {
-  if (startsWith(conditionMessage(c), "We recommend that you use the dev version of ggplot2"))
-    invokeRestart("muffleMessage")
-})
+outdir <-  "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/graphs"
+dir.create(outdir, showWarnings = FALSE)
+ggg <- allgraphs(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/taxa_species.biom", outdir = outdir, sampdepth = 36876)
