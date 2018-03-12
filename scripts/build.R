fddb5152d6c3dcf8654500c89e24db23bec871f7
Poorani <poorani.subramanian@nih.gov>
2018-03-12 13:02:15 -0400

    add colorframe to PCoA
    per feedback from Vishal
    
    
    Former-commit-id: 485b3a147ded68a8c640ff4e89b5a0281acafa6d
    Former-commit-id: dc4472d44ade793cec0d784a4124f50fba3091fb
    Former-commit-id: 96a42d816d4efe5bf7eeeb19a64abe74b65759dd

--- scripts/build.R
+++ scripts/build.R
@@ -1,47 +1,47 @@
 library(devtools)
 library(rmarkdown)
 
-# ## Build package
-# document(roclets=c('rd', 'collate', 'namespace'))
-# install(args = c("--preclean", "--no-multiarch", "--with-keep.source"))
-#
-# ## Imported packages - can check DESCRIPTION
-# ns <- scan("NAMESPACE", sep="\n", what = character())
-# importedpackages <- unique(stringr::str_match(ns, "import.*\\((.*?)[\\,\\)]")[,2])
-#
-# ## Documentation
-#
-# ## library function specification
-# library(Rd2md)
-# yaml <- "---
-# title: \"datavis16s\"
-# output:
-#     github_document:
-#         toc: true
-#         toc_depth: 2
-# ---
-#
-# ```{r setup, include=FALSE}
-# knitr::opts_chunk$set(echo = TRUE, eval = FALSE, tidy=TRUE, tidy.opts = list(width.cutoff=80))
-# ```
-# "
-#
-# mdfile <- "doc/Reference_Manual_datavis16s.md"
-# Rmdfile <- gsub(".md", ".Rmd", mdfile)
-# ## CRAN Rd2md
-# # ReferenceManual(outdir = file.path(getwd(), "doc"), front.matter = yaml)
-# ## My Rd2md https://github.com/pooranis/Rd2md
-# ReferenceManual(outdir = file.path(getwd(), "doc"), front.matter = yaml, title.level = 1, run.examples = FALSE, sepexported = TRUE)
-# file.copy(mdfile, Rmdfile, overwrite = TRUE )
-# render(Rmdfile)
-# file.remove(Rmdfile)
+## Build package
+document(roclets=c('rd', 'collate', 'namespace'))
+install(args = c("--preclean", "--no-multiarch", "--with-keep.source"))
 
-## User docs
-render("doc/user_doc.Rmd", output_format = "github_document")
-file.rename("doc/user_doc.md", "doc/github_doc.md")
-render("doc/user_doc.Rmd", output_format = "html_document", output_file = "datavis16s_pipeline.html")
-system2(command = "sed" , args=c('-i.bak', '\'s/[\\“\\”]/\"/g\'', "doc/datavis16s_pipeline.html"))
-file.remove("doc/datavis16s_pipeline.html.bak")
-file.rename("doc/github_doc.md", "doc/user_doc.md")
-file.remove("doc/user_doc.html")
+## Imported packages - can check DESCRIPTION
+ns <- scan("NAMESPACE", sep="\n", what = character())
+importedpackages <- unique(stringr::str_match(ns, "import.*\\((.*?)[\\,\\)]")[,2])
+
+## Documentation
+
+## library function specification
+library(Rd2md)
+yaml <- "---
+title: \"datavis16s\"
+output:
+    github_document:
+        toc: true
+        toc_depth: 2
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE, eval = FALSE, tidy=TRUE, tidy.opts = list(width.cutoff=80))
+```
+"
+
+mdfile <- "doc/Reference_Manual_datavis16s.md"
+Rmdfile <- gsub(".md", ".Rmd", mdfile)
+## CRAN Rd2md
+# ReferenceManual(outdir = file.path(getwd(), "doc"), front.matter = yaml)
+## My Rd2md https://github.com/pooranis/Rd2md
+ReferenceManual(outdir = file.path(getwd(), "doc"), front.matter = yaml, title.level = 1, run.examples = FALSE, sepexported = TRUE)
+file.copy(mdfile, Rmdfile, overwrite = TRUE )
+render(Rmdfile)
+file.remove(Rmdfile)
+
+# ## User docs
+# render("doc/user_doc.Rmd", output_format = "github_document")
+# file.rename("doc/user_doc.md", "doc/github_doc.md")
+# render("doc/user_doc.Rmd", output_format = "html_document", output_file = "datavis16s_pipeline.html")
+# system2(command = "sed" , args=c('-i.bak', '\'s/[\\“\\”]/\"/g\'', "doc/datavis16s_pipeline.html"))
+# file.remove("doc/datavis16s_pipeline.html.bak")
+# file.rename("doc/github_doc.md", "doc/user_doc.md")
+# file.remove("doc/user_doc.html")
 
