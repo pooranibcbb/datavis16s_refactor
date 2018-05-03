a84a09fa6f2b4d6ad37fea249e4afc8fc81cd003
Poorani <poorani.subramanian@nih.gov>
2018-05-02 23:07:29 -0400

    write tutorial
    Former-commit-id: 51b90cc8a3e211e6b62ccfa2434a1b4fa3968387
    Former-commit-id: a5a3d11ba3e770fb7fba896bd19e994682471514
    Former-commit-id: 66f45ec6c0ca88ad17e566f88070d38a4b6936f5

--- scripts/test.R
+++ scripts/test.R
@@ -1,13 +1,10 @@
 devtools::load_all()
-
-amp <- readindata(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/taxa_species.biom")
-sn <- datavis16s:::shortnames(amp$tax)
 outdir <- "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata"
-morphheatmap(amp = amp, outdir = outdir)
-stop()
+amp <- readindata(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/taxa_species.biom")
+
 unlink(file.path(outdir, "graphs"), recursive = TRUE)
 unlink(file.path(outdir, "logfile.txt"), recursive = TRUE)
-trygraphwrapper(datafile = "testdata/SILVA_OTU_table.biom", outdir = outdir, mapfile = "testdata/SILVA_mapfile.txt", allgraphs,  logfilename = file.path(outdir, "logfile.txt"))
+trygraphwrapper(datafile = "testdata/taxa_species.biom", outdir = outdir, mapfile = "testdata/vanessa_HF_mapfile.txt", allgraphs,  logfilename = file.path(outdir, "logfile.txt"), sampdepth=36876)
 # datafile = "testdata/SILVA_OTU_table.biom"
 # mapfile =  "testdata/SILVA_mapfile.txt"
 # amp <- readindata(datafile=datafile, mapfile = mapfile)
