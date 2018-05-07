81bb53815a7a93f0373baf314900c8f8c4e5313f
Poorani <poorani.subramanian@nih.gov>
2018-05-07 16:09:04 -0400

    modify biobakery to run 4 tasks by default
    Former-commit-id: 73b43ec4babf81dcbe7241f90dd44c3f321bf9e6
    Former-commit-id: f77c934b4de30048320e35a5978865dbee74327f
    Former-commit-id: 21347702dc1dfdd71129f59bcac02da5deba696d

--- scripts/test.R
+++ scripts/test.R
@@ -2,17 +2,13 @@ devtools::load_all()
 outdir <- "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/graphs"
 amp <- readindata(mapfile="testdata/vanessa_HF_mapfile.txt", datafile = "testdata/taxa_species.biom")
 
-adivboxplot(amp = amp, sampdepth = 40000, outdir=outdir)
+allgraphs(datafile = "testdata/taxa_species.biom", outdir=outdir, mapfile="testdata/vanessa_HF_mapfile.txt", sampdepth = 40000)
 
-testfunction <- function() {
-  dv <-   bpexploder(A, settings = list(groupVar = "TreatmentGroup", yVar = "Shannon", tipText = list(SampleID = "SampleID", Shannon = "Shannon Index", Description = "Desc"), relativeWidth = 0.8, levelColors = c("red", "blue")))
-  print(dv$sizingPolicy)
-}
 
-stop()
-unlink(file.path(outdir, "graphs"), recursive = TRUE)
-unlink(file.path(outdir, "logfile.txt"), recursive = TRUE)
-trygraphwrapper(datafile = "testdata/taxa_species.biom", outdir = outdir, mapfile = "testdata/vanessa_HF_mapfile.txt", allgraphs,  logfilename = file.path(outdir, "logfile.txt"), sampdepth=36876)
+
+# unlink(file.path(outdir, "graphs"), recursive = TRUE)
+# unlink(file.path(outdir, "logfile.txt"), recursive = TRUE)
+# trygraphwrapper(datafile = "testdata/taxa_species.biom", outdir = outdir, mapfile = "testdata/vanessa_HF_mapfile.txt", allgraphs,  logfilename = file.path(outdir, "logfile.txt"), sampdepth=36876)
 # datafile = "testdata/SILVA_OTU_table.biom"
 # mapfile =  "testdata/SILVA_mapfile.txt"
 # amp <- readindata(datafile=datafile, mapfile = mapfile)
