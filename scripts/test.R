0f861f1bcbccb48578410833046f4fe0c47eaf0d
Poorani <poorani.subramanian@nih.gov>
2018-05-25 21:29:09 -0400

    fix biomformat generate_matrix
    Former-commit-id: 29601a26dbf38237a8d1040d9e90d31895ae6094
    Former-commit-id: 3852b1bdb0611c4c1e2e63368168f85bb00f760d
    Former-commit-id: 2e8b9ff5e6cbd487a84b770d54d8f59970810bac

--- scripts/test.R
+++ scripts/test.R
@@ -1,16 +1,17 @@
 devtools::load_all()
 outdir <- "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/graphs"
-#amp <- readindata(mapfile="testdata/sjogren_gz_mapfile.txt", datafile = "testdata/taxa_species.biom")
+amp <- readindata(datafile="testdata/otu_table_mc2_w_tax_no_pynast_failures.biom", mapfile = "testdata/easton_corrected.txt")
+
+# library(rhdf5)
 #
-# allgraphs(datafile = "testdata/taxa_species.biom", outdir=outdir, mapfile="testdata/vanessa_HF_mapfile.txt", sampdepth = 40000)
-
-outdir <- "/Users/subramanianp4/git/nephele2/pipelines/datavis16s/testdata/graphsbig"
-# amp <- readindata(mapfile="testdata/complete_corrected.txt", datafile = "testdata/otu_table.biom")
-allgraphs( datafile = "testdata/otu_table.biom",mapfile="testdata/complete_corrected.txt", outdir = outdir, sampdepth = 20969 )
-
-#Sys.setlocale('LC_ALL','C')
-amp <- readindata(mapfile="testdata/complete_corrected.txt", datafile = "testdata/otu_table.biom")
-#morphheatmap(amp = amp, sampdepth=19058, outdir = outdir)
-
-
-
+# infile <- "testdata/otu_table_mc2_w_tax_no_pynast_failures.biom"
+# H5close()
+# H5garbage_collect()
+# #tax <- h5read(infile, "/observation/metadata/taxonomy")
+# fid <- H5Fopen(infile)
+# obs <- H5Gopen(fid, "observation")
+# obsmet <- H5Gopen(obs, "metadata")
+# taxopt <- H5Dopen(obsmet, "taxonomy")
+# tax <- H5Dread(taxopt)
+# #H5close()
+# H5garbage_collect()
