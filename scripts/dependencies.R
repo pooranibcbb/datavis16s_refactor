6c645fe44b52b00275b6325440fc5344550b529a
Poorani <poorani.subramanian@nih.gov>
2019-01-07 16:03:38 -0500

    update dependencies for datavis
    Former-commit-id: a43e1383aa08fbe476a8e44529e846bb5d5926c5
    Former-commit-id: 8a86673aaf1777c9333dbe73bf6c193d69321ef5
    Former-commit-id: 83938a8670fa887c0d700eb81fd345014ec5bb8c

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 
 ## Nephele is using R 3.4.x. The following script only works with this version!
-if (as.numeric(version$major) > 3 || as.numeric(version$minor) >= 5) stop("This package only works with R version 3.4.x.  The version you are using is not compatible.")
+if (as.numeric(version$major) > 3 || as.numeric(version$minor) >= 6 || as.numeric(version$minor) < 5) stop("This package only works with R version 3.5.x.  The version you are using is not compatible.")
 
 options(repos=c(CRAN="https://cran.rstudio.com/"))
 
-## Bioconductor 3.6
-if (!"BiocInstaller" %in% installed.packages() || packageVersion("BiocInstaller") != "1.28.0") install.packages("BiocInstaller", repos="http://bioconductor.org/packages/3.6/bioc")
-library(BiocInstaller)
-if (!"biomformat" %in% installed.packages() || packageVersion("biomformat") != "1.6.0" ) biocLite('biomformat')
+## Bioconductor 3.8
+if (!("BiocManager" %in% installed.packages()) || packageVersion("BiocManager") != "1.30.4") install.packages("BiocManager")
+
+if (!"biomformat" %in% installed.packages() || packageVersion("biomformat") != "1.10.1" ) BiocManager::install('biomformat', version="3.8")
 
 
 ## devtools
@@ -18,6 +18,6 @@ install_github('cmap/morpheus.R', ref="7ce5f6a3fdb947dba9014115b4c324fcd7ec7f5d"
 install_github("MadsAlbertsen/ampvis2", dependencies=TRUE, ref="66dec692dccc28d25a034f7f6eab8ca16bfd9165")
 install_github("homerhanumat/bpexploder", ref="ae46f7f3753728795c857c37f2608eb7ea3a92df", dependencies=TRUE)
 ## change directory path
-install_local("/Users/subramanianp4/git/nephele2/pipelines/datavis16s", dependencies=c("Depends", "Imports"), force=T)
+install_local("/usr/local/src/nephele2/pipelines/datavis16s", dependencies=c("Depends", "Imports"), force=T)
 
 
